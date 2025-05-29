a062afddef83d32cdbafb652749aeae4ec5f0344
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2025-05-29 18:56:47 -0300

    asdf

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 
-FROM python:3.9-slim
+FROM python:3.13-slim
 
 WORKDIR /app
 
@@ -9,11 +9,9 @@ RUN apt-get update && apt-get install -y \
     software-properties-common \
     git \
     && rm -rf /var/lib/apt/lists/*
-
-RUN git clone https://github.com/streamlit/streamlit-example.git .
-
-RUN pip3 install -r requirements.txt
-
+COPY requirements_beta.txt .  
+RUN pip3 install -r requirements_beta.txt
+COPY .  .
 EXPOSE 8501
 
 HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
--- app.py
+++ app.py
@@ -5,15 +5,15 @@ from qdrant_client import QdrantClient, models
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-client = QdrantClient(
-	url=st.secrets.qdrant.url,
-	api_key=st.secrets.qdrant.key
-)
-
 # client = QdrantClient(
-# 	host = "localhost",
-# 	port= 6333	
+# 	url=st.secrets.qdrant.url,
+# 	api_key=st.secrets.qdrant.key
 # )
+
+client = QdrantClient(
+	host = "localhost",
+	port= 6333	
+)
 model = SentenceTransformer("sentence-transformers/multi-qa-MiniLM-L6-cos-v1")
 # model = model.to("cpu")
 _ = model.encode("test", normalize_embeddings=True) 
@@ -21,7 +21,7 @@ _ = model.encode("test", normalize_embeddings=True)
 
 st.title('Busqueda GV 2.0')
 
-query = st.text_input("Escribir terminos de busqueda separados por '," )
+query = st.text_input("Escribir terminos de busqueda separados por ','" )
 documents = query.split(",")
 for query in documents:
 	if query != "":
