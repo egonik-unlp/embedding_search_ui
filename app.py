b2e895722028d013a7eedbb02061dab8776c3c64
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2025-06-02 10:40:44 -0300

    ammend runnable

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ from qdrant_client import QdrantClient, models
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-# client = QdrantClient(
-# 	url=st.secrets.qdrant.url,
-# 	api_key=st.secrets.qdrant.key
-# )
-
 client = QdrantClient(
-	host = "localhost",
-	port= 6333	
-)
+ 	url=st.secrets["qdrant"]["url"],
+ 	api_key=st.secrets["qdrant"]["key"]
+ )
+
+#client = QdrantClient(
+#	host = "localhost",
+#	port= 6333	
+#)
 model = SentenceTransformer("sentence-transformers/multi-qa-MiniLM-L6-cos-v1")
 # model = model.to("cpu")
 _ = model.encode("test", normalize_embeddings=True) 
