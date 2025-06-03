e3db8eff70d45869c1423346fc90922eaba1d48b
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2025-06-03 12:48:36 -0300

    funciona local

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from qdrant_client import QdrantClient, models
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
+
+
+
 client = QdrantClient(
  	url=st.secrets["qdrant"]["url"],
  	api_key=st.secrets["qdrant"]["key"]
@@ -15,26 +18,26 @@ client = QdrantClient(
 #	port= 6333	
 #)
 model = SentenceTransformer("sentence-transformers/multi-qa-MiniLM-L6-cos-v1")
-# model = model.to("cpu")
+# model_name = "sentence-transformers/all-MiniLM-L6-v2"
+
+
+
+
+model = model.to("cpu")
 _ = model.encode("test", normalize_embeddings=True) 
+def busqueda(documents):
+	for query in documents:
+		print(query)
+		if query != "":
+			query_embedding = model.encode(query, normalize_embeddings=True)
 
-
-st.title('Busqueda GV 2.0')
-
-query = st.text_input("Escribir terminos de busqueda separados por ','" )
-documents = query.split(",")
-for query in documents:
-	if query != "":
-		query_embedding = model.encode(query, normalize_embeddings=True)
-
-		# Perform search
-		results = client.search(
-		collection_name="ingredientes",
-		query_vector=query_embedding.tolist(), 
-		limit=1
-	)
-
-	
+			# Perform search
+			results = client.search(
+			collection_name="ingredientes",
+			query_vector=query_embedding.tolist(), 
+			limit=1
+		)
+			
 		for result in results:
 			res = result.payload, f"query => {query}→ Score: {result.score:.3f}"
 			x = result.payload
@@ -42,3 +45,40 @@ for query in documents:
 			x["score"] = result.score
 			st.json(result.payload)
 			print(res)
+
+def busqueda_mas75(documents):
+	for query in documents:
+		if query != "":
+			query_embedding = model.encode(query, normalize_embeddings=True)
+
+			# Perform search
+			results = client.search(
+			collection_name="ingredientes",
+			query_vector=query_embedding.tolist(), 
+			limit=1
+		)
+
+	
+		for result in results:
+			res = result.payload, f"query => {query}→ Score: {result.score:.3f}"
+			x = result.payload
+			x["query"] = query
+			x["score"] = result.score
+			if result.score > 0.75:
+				st.json(result.payload)
+				print(res)
+
+
+st.title('Busqueda GV 2.0')
+
+# query = st.text_input()
+
+query = st.text_area("Escribir terminos de busqueda separados por ','" )
+documents = query.split(",")
+if st.button("busqueda"):
+	busqueda(documents)
+if st.button("busqueda mayor similitud"):
+	busqueda_mas75(documents)
+
+
+
