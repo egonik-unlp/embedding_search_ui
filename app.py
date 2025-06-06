9ba21b9d1651d71e7a2a28a99a16daed343d1c9a
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2025-06-06 11:37:51 -0300

    anda

--- app.py
+++ app.py
@@ -5,26 +5,19 @@ from qdrant_client import QdrantClient, models
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-
-
-
 client = QdrantClient(
  	url=st.secrets["qdrant"]["url"],
  	api_key=st.secrets["qdrant"]["key"]
  )
 
-#client = QdrantClient(
-#	host = "localhost",
-#	port= 6333	
-#)
-model = SentenceTransformer("sentence-transformers/multi-qa-MiniLM-L6-cos-v1")
-# model_name = "sentence-transformers/all-MiniLM-L6-v2"
-
-
-
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 model = model.to("cpu")
 _ = model.encode("test", normalize_embeddings=True) 
+
+def busqueda_metodo_anterior(documents):
+	return "todo"
+
 def busqueda(documents):
 	for query in documents:
 		print(query)
@@ -41,13 +34,14 @@ def busqueda(documents):
 		for result in results:
 			res = result.payload, f"query => {query}→ Score: {result.score:.3f}"
 			x = result.payload
-			x["query"] = query
+			x["query"] = query # type: ignore
 			x["score"] = result.score
 			st.json(result.payload)
 			print(res)
 
 def busqueda_mas75(documents):
 	for query in documents:
+		print(query)
 		if query != "":
 			query_embedding = model.encode(query, normalize_embeddings=True)
 
@@ -68,17 +62,15 @@ def busqueda_mas75(documents):
 				st.json(result.payload)
 				print(res)
 
-
-st.title('Busqueda GV 2.0')
-
-# query = st.text_input()
-
-query = st.text_area("Escribir terminos de busqueda separados por ','" )
-documents = query.split(",")
-if st.button("busqueda"):
-	busqueda(documents)
-if st.button("busqueda mayor similitud"):
-	busqueda_mas75(documents)
+def main():
+	st.title('Busqueda GV 2.0')
+	query = st.text_area("Escribir terminos de busqueda separados por ','" )
+	documents = query.split(",")
+	if st.button("busqueda"):
+		busqueda(documents)
+	if st.button("busqueda mayor similitud"):
+		busqueda_mas75(documents)
 
 
-
+if __name__ == "__main__":
+	main()
